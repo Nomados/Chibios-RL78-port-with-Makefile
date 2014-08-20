793c2c0aba1f695ba8d38aab46cb421b95fcb0e2
Arthur Ganeev <iishere@ya.ru>
2014-08-20 10:33:38 +0900

    Update rules.mk

--- os/ports/GCC/RL78/rules.mk
+++ os/ports/GCC/RL78/rules.mk
@@ -14,7 +14,7 @@ SRC        = $(CSRC)$(CPPSRC)
 # Object files groups
 COBJS   = $(CSRC:.c=.o)
 CPPOBJS = $(CPPSRC:.cpp=.o)
-#ASMOBJS = $(ASMSRC:.S=.o)
+ASMOBJS = $(ASMSRC:.S=.o)
 OBJS   = $(ASMOBJS) $(COBJS) $(CPPOBJS)
 
 # Paths
@@ -30,20 +30,16 @@ LIBS    = $(DLIBS) $(ULIBS)
 
 MCFLAGS = 
 ODFLAGS   = -x --syms
-ASFLAGS = $(MCFLAGS) -Wa,-amhls=$(<:.s=.lst) $(ADEFS)
+ASFLAGS = $(MCFLAGS) -Wa,-amhls=$(<:.S=.lst) $(ADEFS)
 CPFLAGS = $(MCFLAGS) $(OPT) $(COPT) $(WARN) -Wa,-alms=$(<:.c=.lst) $(DEFS)
 
-	LDFLAGS = -Wl,-Map=$(PROJECT).map -Wl,--relax -Wl,--gc-sections -nostartfiles
-
-	
-# Generate dependency information
-ASFLAGS += -MD -MP -MF .dep/$(@F).d
-CPFLAGS += -MD -MP -MF .dep/$(@F).d
+LDFLAGS = -nostartfiles -Wl,--relax -Wl,--gc-sections -T $(LDSCRIPT)
 
 #
 # Makefile rules
 #
 all: $(OBJS) $(PROJECT).elf $(PROJECT).hex $(PROJECT).bin $(PROJECT).dmp MAKE_ALL_RULE_HOOK
+	rl78-elf-size $(PROJECT).elf
 
 MAKE_ALL_RULE_HOOK:
 
@@ -56,13 +52,15 @@ $(COBJS) : %.o : %.c
 	@echo
 	$(CC) -c $(CPFLAGS) -I . $(IINCDIR) $< -o $@
 
-$(ASMOBJS) : 
+$(ASMOBJS) : %.o : %.S
 	@echo
-	$(AS) -c  -o 
+	$(AS) -c $(ASFLAGS) -I . $(IINCDIR) $< -o $@
+
 
 %elf: $(OBJS)
-	@echo
+	@echo 
 	$(LD) $(OBJS) $(LDFLAGS) $(LIBS) -o $@
+
 
 %hex: %elf
 	$(HEX) $< $@
@@ -77,11 +75,5 @@ clean:
 	-rm -f $(OBJS)
 	-rm -f $(CSRC:.c=.lst) $(CPPSRC:.cpp=.lst) $(ASMSRC:.S=.lst)
 	-rm -f $(PROJECT).elf $(PROJECT).dmp $(PROJECT).map $(PROJECT).hex $(PROJECT).bin
-	-rm -fR .dep
-
-#
-# Include the dependency files, should be the last of the makefile
-#
-include $(shell mkdir .dep 2>/dev/null) $(wildcard .dep/*)
 
 # *** EOF ***
