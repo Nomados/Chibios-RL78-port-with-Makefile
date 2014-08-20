1586731143da7686ea5f6756021e8fbcdb671e31
Arthur Ganeev <iishere@ya.ru>
2014-08-20 10:33:16 +0900

    Update port.mk

--- os/ports/GCC/RL78/port.mk
+++ os/ports/GCC/RL78/port.mk
@@ -1,5 +1,6 @@
 # List of the ChibiOS/RT RL78 port files.
-PORTSRC =
+PORTSRC = ${CHIBIOS}/os/ports/GCC/RL78/chcore.c \
+          ${CHIBIOS}/os/ports/GCC/RL78/vectors.c
 
 PORTASM = ${CHIBIOS}/os/ports/GCC/RL78/crt0.S 
 
